d8b87c126946ec2e8fa9a36ee234d470ac8dc09e
T Saket Hatwar <112693309+SKADE2303@users.noreply.github.com>
2024-07-01 23:07:40 +0530

    changed remappings

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
     uint256 internal s_tokenCounter;
