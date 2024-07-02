bf16e86e27b0a280d38926b68f2e273d69bf7327
T Saket Hatwar <112693309+SKADE2303@users.noreply.github.com>
2024-07-02 22:53:15 +0530

    Create script to deploy MoodNFT

---
+++ script/DeployMoodNFT.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
+
+contract DeployMoodNFT is Script {
+    function run() external returns (MoodNFT) {
+        vm.startBroadcast();
+        BasicNFT basicNFT = new BasicNFT();
+        vm.stopBroadcast();
+        return basicNFT;
+    }
+
+    function svgToURI(string memory svg) public pure returns(string memory){
+
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg))) 
+        );
+        return string(abi.encodePacked(baseURI, svgBase64Encoded));
+
+    }  
+}
