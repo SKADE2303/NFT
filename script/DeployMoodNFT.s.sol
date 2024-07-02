c069a76561b95c41c7488a0277253d4c2e9adec9
T Saket Hatwar <112693309+SKADE2303@users.noreply.github.com>
2024-07-03 00:04:58 +0530

    updated script and test

--- script/DeployMoodNFT.s.sol
+++ script/DeployMoodNFT.s.sol
@@ -7,19 +7,15 @@ import {Base64} from "../lib/openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract DeployMoodNFT is Script {
     function run() external returns (MoodNFT) {
-        vm.startBroadcast();
-        BasicNFT basicNFT = new BasicNFT();
-        vm.stopBroadcast();
-        return basicNFT;
+        string memory sadSVG = vm.readFile("./img/sad.svg");
+        string memory happySVG = vm.readFile("./img/happy.svg");
     }
 
-    function svgToURI(string memory svg) public pure returns(string memory){
-
-        string memory baseURL = "data:image/svg+xml;base64,";
+    function svgToURI(string memory svg) public pure returns (string memory) {
+        string memory baseURI = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(
-            bytes(string(abi.encodePacked(svg))) 
+            bytes(string(abi.encodePacked(svg)))
         );
         return string(abi.encodePacked(baseURI, svgBase64Encoded));
-
-    }  
+    }
 }
